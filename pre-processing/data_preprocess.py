aca449f394a94af8e3090673b213fbbb5864678e
CYS <cys@CYSdeMacBook-Pro-2.local>
2020-09-19 19:24:02 +0800

    data_preprocess

--- pre-processing/data_preprocess.py
+++ pre-processing/data_preprocess.py
@@ -79,131 +79,51 @@ def getAlldata(data_folder, train_save_path, test_save_path):
                 test_race_num[alldatasets[data][index]['race']][alldatasets[data][index]['age']]+=1
     print(train_race_num,test_race_num)
 
-def RaceSpecificData():
-    f=open('/home/david/aibias/datasets/alldata.json','r')
-    alldatasets=json.load(f)
-    allraces={
-        'caucasian':[],
-        'afroamerican':[],
-        'asian':[]
-    }
-    age_num=defaultdict(int)
-    race_num={
-        'caucasian':copy.deepcopy(age_num),
-        'afroamerican':copy.deepcopy(age_num),
-        'asian':copy.deepcopy(age_num)
-    }
-    for data in alldatasets:
-        for samples in tqdm(alldatasets[data]):
-            if 1<=samples['age']<=101 and samples['race']in ['caucasian','afroamerican','asian']:
-                if race_num[samples['race']][samples['age']]>=1000:
-                    continue
-                allraces[samples['race']].append({'image_path':samples['image_path'],'age':int(samples['age']),'gender':samples['gender'],'race':samples['race']})
-                race_num[samples['race']][samples['age']]+=1
-    
-    print(race_num)
-    train_race_num={
-        'caucasian':copy.deepcopy(age_num),
-        'afroamerican':copy.deepcopy(age_num),
-        'asian':copy.deepcopy(age_num)
-    }
-    test_race_num={
-        'caucasian':copy.deepcopy(age_num),
-        'afroamerican':copy.deepcopy(age_num),
-        'asian':copy.deepcopy(age_num)
-    }
-    
-    train_size=int(750*0.8)
-    
-    for data in allraces:
-        if not os.path.exists('/home/david/aibias/datasets/1_101_1yr/{}'.format(data)):
-            os.makedirs('/home/david/aibias/datasets/1_101_1yr/{}'.format(data))
-        train_info=open(os.path.join('/home/david/aibias/datasets/1_101_1yr/{}'.format(data),('RaceFineTuneData'+'_train_info_'+str(1)+'yr.txt')),'w')
-        test_info=open(os.path.join('/home/david/aibias/datasets/1_101_1yr/{}'.format(data),('RaceFineTuneData'+'_test_info_'+str(1)+'yr.txt')),'w')
-        indices=[i for i in range(len(allraces[data]))]
-        random.shuffle(indices)
-        for index in tqdm(indices):
-            if allraces[data][index]['race'] not in ['caucasian','afroamerican','asian']:
-                continue
-            if allraces[data][index]['age']<1 or allraces[data][index]['age']>100:
-                continue
-            if train_race_num[allraces[data][index]['race']][allraces[data][index]['age']]<\
-                int(race_num[allraces[data][index]['race']][allraces[data][index]['age']]*0.8):
-                train_info.write('{}\t{}\t{}\t{}\n'.format(allraces[data][index]['image_path'],allraces[data][index]['race'],allraces[data][index]['age']-1,allraces[data][index]['gender']))
-                train_race_num[allraces[data][index]['race']][allraces[data][index]['age']]+=1
-            elif test_race_num[allraces[data][index]['race']][allraces[data][index]['age']]<\
-                race_num[allraces[data][index]['race']][allraces[data][index]['age']]-int(race_num[allraces[data][index]['race']][allraces[data][index]['age']]*0.8):
-                test_info.write('{}\t{}\t{}\t{}\n'.format(allraces[data][index]['image_path'],allraces[data][index]['race'],allraces[data][index]['age']-1,allraces[data][index]['gender']))
-                test_race_num[allraces[data][index]['race']][allraces[data][index]['age']]+=1
-    print(train_race_num,test_race_num)
+def getMinMaxSample(num_sample):
+    max_sample = 0
+    min_sample = np.inf
+    for key in num_sample:
+        max_sample = max(max_sample,np.quantile(num_sample[key].values(),0.8))
+        min_sample = min(min_sample,np.quantile(num_sample[key].values(),0.2))
+    return min_sample, max_sample
 
-def getUnbalenceddata(opt):
-    f=open('/home/david/aibias/datasets/alldata.json','r')
-    alldatasets=json.load(f)
-    allraces={
+def getBalancedData():
+    
+    # Get all four datasets
+    alldatasets = {
+    'UTKdata':getUTKdata(data_folder),
+    'Megaasian': getMegaasianData(data_folder),
+    'APPA': getAPPAdata(data_folder),
+    'MORPH': getMORPHdata(data_folder)
+    }
+
+    datasets = {
         'caucasian':defaultdict(list),
         'afroamerican':defaultdict(list),
         'asian':defaultdict(list)
     }
-    age_num=defaultdict(int)
-    race_num={
-        'caucasian':copy.deepcopy(age_num),
-        'afroamerican':copy.deepcopy(age_num),
-        'asian':copy.deepcopy(age_num)
+
+    # Number of samples for each ethnicity in each age
+    num_sample = {
+        'caucasian':defaultdict(int),
+        'afroamerican':defaultdict(int),
+        'asian':defaultdict(int)
     }
+
     for data in alldatasets:
         for samples in tqdm(alldatasets[data]):
-            if 1<=samples['age']<=101 and samples['race']in ['caucasian','asian']:
-                if race_num[samples['race']][samples['age']]>=1000:
-                    continue
-                allraces[samples['race']][samples['age']].append([samples['image_path'],samples['age'],samples['race'],samples['gender']])
-                race_num[samples['race']][samples['age']]+=1
-            if 1<=samples['age']<=101 and samples['race']=='afroamerican':
-                if race_num[samples['race']][samples['age']]>=200:
-                    continue
-                allraces[samples['race']][samples['age']].append([samples['image_path'],samples['age'],samples['race'],samples['gender']])
-                race_num[samples['race']][samples['age']]+=1
-    for key in race_num['caucasian']:
-        race_num['asian'][key]=min(race_num['asian'][key],race_num['caucasian'][key])
-        race_num['caucasian'][key]=race_num['asian'][key]
-    for key in race_num['caucasian']:
-        print(race_num['asian'][key],race_num['caucasian'][key])
-    train_race_num={
-        'caucasian':copy.deepcopy(age_num),
-        'afroamerican':copy.deepcopy(age_num),
-        'asian':copy.deepcopy(age_num)
-    }
-    test_race_num={
-        'caucasian':copy.deepcopy(age_num),
-        'afroamerican':copy.deepcopy(age_num),
-        'asian':copy.deepcopy(age_num)
-    }
-    if not os.path.exists('/home/david/aibias/datasets/1_101_1yr/Unbalanced'):
-        os.makedirs('/home/david/aibias/datasets/1_101_1yr/Unbalanced')
-    train_info=open(os.path.join('/home/david/aibias/datasets/1_101_1yr/Unbalanced',('FineTuneData'+'_train_info_'+str(1)+'yr.txt')),'w')
-    test_info=open(os.path.join('/home/david/aibias/datasets/1_101_1yr/Unbalanced',('FineTuneData'+'_test_info_'+str(1)+'yr.txt')),'w')
-    
-    for data in alldatasets:
-        indices=[i for i in range(len(alldatasets[data]))]
-        random.shuffle(indices)
-        for index in tqdm(indices):
-            if alldatasets[data][index]['race'] not in ['caucasian','afroamerican','asian']:
-                continue
-            if alldatasets[data][index]['age']<1 or alldatasets[data][index]['age']>101:
-                continue
-            if train_race_num[alldatasets[data][index]['race']][alldatasets[data][index]['age']]<int(race_num[alldatasets[data][index]['race']][alldatasets[data][index]['age']]*0.8):
-                if alldatasets[data][index]['age']>=98:
-                    train_info.write('{}\t{}\t{}\t{}\n'.format(alldatasets[data][index]['image_path'],alldatasets[data][index]['race'],alldatasets[data][index]['age']-2,alldatasets[data][index]['gender']))
-                else:
-                    train_info.write('{}\t{}\t{}\t{}\n'.format(alldatasets[data][index]['image_path'],alldatasets[data][index]['race'],alldatasets[data][index]['age']-1,alldatasets[data][index]['gender']))
-                train_race_num[alldatasets[data][index]['race']][alldatasets[data][index]['age']]+=1
-            elif test_race_num[alldatasets[data][index]['race']][alldatasets[data][index]['age']]<race_num[alldatasets[data][index]['race']][alldatasets[data][index]['age']]-int(race_num[alldatasets[data][index]['race']][alldatasets[data][index]['age']]*0.8):
-                if alldatasets[data][index]['age']>=98:
-                    test_info.write('{}\t{}\t{}\t{}\n'.format(alldatasets[data][index]['image_path'],alldatasets[data][index]['race'],alldatasets[data][index]['age']-2,alldatasets[data][index]['gender']))
-                else:
-                    test_info.write('{}\t{}\t{}\t{}\n'.format(alldatasets[data][index]['image_path'],alldatasets[data][index]['race'],alldatasets[data][index]['age']-1,alldatasets[data][index]['gender']))
-                test_race_num[alldatasets[data][index]['race']][alldatasets[data][index]['age']]+=1
-    print(train_race_num.items(),test_race_num.items())
+            if 1<=samples['age']<=101 and samples['race'] in ['caucasian','afroamerican','asian']:
+                datasets[samples['race']][samples['age']].append([samples['image_path'],samples['age'],samples['race'],samples['gender']])
+                num_sample[samples['race']][samples['age']]+=1
+
+    for key in num_sample:
+        samples = copy.deepcopy(num_sample[key])
+        num_sample[key] = dict(sorted(samples.items(), key=lambda samples:samples[1]))
+
+    min_threshold,max_threshold = getMinMaxSample(num_sample)
+
+    # for age in 
+    return 
 
 
 def main(data_folder):
